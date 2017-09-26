e6ccdef76a4bc5a285c972b5c393fee90edfbef2
Shibhansh Dohare <shibhanshdohare1997@gmail.com>
2017-09-27 03:38:19 +0530

    add/coreference resolution for doucments

---
+++ resolve_documents.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import json
+import numpy as np
+import pickle
+
+import cgi
+import BaseHTTPServer
+import ssl
+
+import tensorflow as tf
+import coref_model as cm
+import util
+
+import nltk
+nltk.download("punkt")
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def create_example(text):
+  # This is where tokenization is taking place
+  # need to edit here to coref resolution of multiple documents in a single file
+
+  raw_sentences = sent_tokenize(text)
+  sentences = [word_tokenize(s) for s in raw_sentences]
+  speakers = [["" for _ in sentence] for sentence in sentences]
+  return {
+    "doc_key": "nw",
+    "clusters": [],
+    "sentences": sentences,
+    "speakers": speakers,
+  }
+
+def make_predictions(text, model):
+  example = create_example(text)
+  tensorized_example = model.tensorize_example(example, is_training=False)
+  feed_dict = {i:t for i,t in zip(model.input_tensors, tensorized_example)}
+  a, b, c, mention_starts, mention_ends, antecedents, antecedent_scores, head_scores = \
+                                        session.run(model.predictions + [model.head_scores], feed_dict=feed_dict)
+
+  predicted_antecedents = model.get_predicted_antecedents(antecedents, antecedent_scores)
+
+  example["predicted_clusters"], mention_to_predicted = \
+                                model.get_predicted_clusters(mention_starts, mention_ends, predicted_antecedents)
+
+  example["top_spans"] = zip((int(i) for i in mention_starts), (int(i) for i in mention_ends))
+  example["head_scores"] = head_scores.tolist()
+  return example
+
+if __name__ == "__main__":
+  util.set_gpus()
+
+  name = sys.argv[1]
+
+  print "Running experiment: {}.".format(name)
+  config = util.get_config("experiments.conf")[name]
+  config["log_dir"] = util.mkdirs(os.path.join(config["log_root"], name))
+
+  util.print_config(config)
+  model = cm.CorefModel(config)
+
+  saver = tf.train.Saver()
+  log_dir = config["log_dir"]
+
+  with tf.Session() as session:
+    checkpoint_path = os.path.join(log_dir, "model.max.ckpt")
+    saver.restore(session, checkpoint_path)
+
+    file_name = sys.argv[2]
+
+    with open(file_name,'r') as f:
+      temp = f.readlines()
+      documents = [doc for doc in temp if len(doc.strip()) != 0]
+
+    resovled_documents = []
+    for doc in documents:
+      text = doc.strip()
+      output = make_predictions(text, model)
+      resovled_documents.append([output["predicted_clusters"],util.flatten(output["sentences"])])
+
+    print resovled_documents
+    with open('predicted_resolutions.txt','w') as f:
+      pickle.dump(resovled_documents,f)
